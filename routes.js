083eb24ccf7847f543ad57c48ad7d1b2df24a3dc
rokhmad92 <rokhmad92@gmail.com>
2025-08-14 16:28:04 +0700

    feat: membaca pesan sudah di baca apa belum

--- routes.js
+++ routes.js
@@ -29,19 +29,26 @@ router.post('/message', async (req, res) => {
 
   try {
     const isRegistered = await client.isRegisteredUser(phone);
-    if (!isRegistered) return res.status(400).json({ status: 400, message: `Nomor tidak terdaftar di WhatsApp - ${nomer}` });
+    if (!isRegistered) return res.status(200).json({ status: 400, message: `Nomor tidak terdaftar di WhatsApp - ${nomer}` });
+
+    let message = null;
 
     if (image) {
+      // Form URL
       const media = await MessageMedia.fromUrl(image);
       await client.sendMessage(phone, media, { caption: pesan });
     } else {
-      await client.sendMessage(phone, pesan);
+      message = await client.sendMessage(phone, pesan);
     }
 
-    return res.status(201).json({ status: 201, message: `Berhasil kirim pesan - ${nomer}` });
+    return res.status(201).json({
+      status: 201,
+      message: `Berhasil kirim pesan - ${nomer}`,
+      message_id: message.id._serialized,
+    });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ status: 500, message: `Terjadi kesalahan - ${nomer} - ${error}` });
+    return res.status(200).json({ status: 500, message: `Terjadi kesalahan - ${nomer} - ${error}` });
   }
 });
 
--- whatsapp.js
+++ whatsapp.js
@@ -1,4 +1,5 @@
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+const axios = require('axios');
 
 let qrString = null;
 
@@ -11,8 +12,22 @@ const client = new Client({
   authStrategy: new LocalAuth({ dataPath: 'authSession' }),
 });
 
-client.on('qr', (qr) => qrString = qr);
-client.on('ready', () => qrString = null);
+client.on('qr', (qr) => (qrString = qr));
+client.on('ready', () => (qrString = null));
+
+// update status jika user sudah membaca pesan
+client.on('message_ack', async (msg, ack) => {
+  if (ack != 1) {
+    try {
+      await axios.post('http://localhost:8000/api/update-message-status', {
+        message_id: msg.id._serialized,
+        status: ack,
+      });
+    } catch (err) {
+      console.error('Gagal update status ke Laravel:', err.message);
+    }
+  }
+});
 
 client.initialize();
 
